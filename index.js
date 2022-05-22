032ee0ab81b204b7dcf38a2d4b0a59606eae4c09
Connor Bell (Makeshift) <git@connor-bell.com>
2022-05-22 23:44:47 +0100

    Add actions to build containers, fixed up examples and cleaned up code

--- index.js
+++ index.js
@@ -1,105 +1,112 @@
 const config = {
-    docker_conf: process.env["docker_conf"] ? JSON.parse(process.env["docker_conf"]) : {socketPath: '/var/run/docker_conf.sock'},
-    conf_dir: process.env["conf_dir"] || "/conf",
-    suffix: process.env["suffix"] || "standard",
-    template: process.env["template"] || "./template.vhost",
-    destination: process.env["destination"]
+  docker_conf: process.env.docker_conf ? JSON.parse(process.env.docker_conf) : { socketPath: '/var/run/docker_conf.sock' },
+  conf_dir: process.env.conf_dir || '/conf',
+  suffix: process.env.suffix || 'standard',
+  template: process.env.template || './templates/template.vhost',
+  destination: process.env.destination
 }
 
-const fs = require('fs').promises;
-const path = require('path');
-const {Docker} = require('node-docker-api');
-const {execSync} = require('child_process');
+const fs = require('fs').promises
+const path = require('path')
+const { Docker } = require('node-docker-api')
+const { execSync } = require('child_process')
 
 const promisifyStream = stream => new Promise((resolve, reject) => {
-    stream.on('data', data => ["die", "start"].includes(JSON.parse(data.toString()).status) ? setTimeout(start, 5000) : "")
-    stream.on('end', resolve)
-    stream.on('error', reject)
+  stream.on('data', data => ['die', 'start'].includes(JSON.parse(data.toString()).status) ? setTimeout(start, 5000) : '')
+  stream.on('end', resolve)
+  stream.on('error', reject)
 })
 
 const docker = new Docker(config.docker_conf)
 
 docker.events()
-    .then(stream => promisifyStream(stream))
-    .catch(error => console.log(error));
+  .then(stream => promisifyStream(stream))
+  .catch(error => console.log(error))
 
-async function start() {
-    //rm all files
-    await deleteConfigFiles();
-    //Generate new ones
-    await generateAllFiles();
-    //Send sighup
-    await sendSigHup();
+async function start () {
+  // rm all files
+  await deleteConfigFiles()
+  // Generate new ones
+  await generateAllFiles()
+  // Send sighup
+  await sendSigHup()
 }
 
-async function deleteConfigFiles() {
-    let files = await fs.readdir(path.normalize(config.conf_dir));
-    await Promise.all(files.map(async file => {
-        if (file.endsWith(`.${config.suffix}.conf`)) {
-            await fs.unlink(path.join(config.conf_dir, file));
-        }
-    }))
-}
-
-async function sendSigHup() {
-    let notify = (await docker.container.list()).filter(container => "proxy.notify" in container.data.Labels);
-    console.log(notify)
-    await Promise.all(notify.map(async container => await container.kill({signal: "HUP"})));
-}
-
-async function generateAllFiles() {
-    console.log("Getting list of containers")
-    let containers = await docker.container.list();
-    await Promise.all(containers.map(async container => {
-        if ("proxy.hosts" in container.data.Labels) {
-            console.log(`Found container for proxying ${container.data.Names[0]}`)
-            await generateHostFile(container)
-        }
-    }))
-}
-
-async function generateHostFile(container) {
-    //console.log(container.data.NetworkSettings.Networks)
-    let serverNamesArray = container.data.Labels["proxy.hosts"].split(",")
-    let server_names = serverNamesArray.join("  ");
-    let containerPort = Number(container.data.Labels["proxy.port"]) || 80;
-    //External
-    let proxy_pass;
-
-    let isHostNetworking = 'host' in container.data.NetworkSettings.Networks;
-
-    if (config.destination) {
-        //External
-        let ports = container.data.Ports.filter(port => port.PrivatePort === containerPort);
-        //If there's a private port that matches our container port, then we grab the public port
-        //Else, we use the given container port as it's probably in host networking mode
-        let hostPort = ports.length > 0 ? ports[0].PublicPort : containerPort;
-
-        proxy_pass = `http://${config.destination}:${hostPort}`;
-    } else if (isHostNetworking) {
-        //Local host networking
-        //Do some hackery to get the host IP
-        let hostIp = execSync("route | awk '/^default/ { print $2 }'").toString().trim();
-        proxy_pass = `http://${hostIp}:${containerPort}`
-    } else {
-        //Local and in same network
-        proxy_pass = `http://${container.data.Names[0].split("/")[1]}:${containerPort}`
+async function deleteConfigFiles () {
+  const files = await fs.readdir(path.normalize(config.conf_dir))
+  await Promise.all(files.map(async file => {
+    if (file.endsWith(`.${config.suffix}.conf`)) {
+      await fs.unlink(path.join(config.conf_dir, file))
     }
-    let templateFile = "proxy.template" in container.data.Labels ? container.data.Labels["proxy.template"] : config.template;
-    let template = (await fs.readFile(templateFile)).toString();
-    let vhost = template.interpolate({
-        server_names: server_names,
-        proxy_pass: proxy_pass,
-        is_public: "proxy.isPublic" in container.data.Labels ? "allow 0.0.0.0/0;" : "",
-    })
-    console.log(vhost)
-    await fs.writeFile(`${path.join(config.conf_dir, serverNamesArray[0])}.${config.suffix}.conf`, vhost);
+  }))
 }
 
+async function sendSigHup () {
+  const notify = (await docker.container.list()).filter(container => 'proxy.notify' in container.data.Labels)
+  console.log(notify)
+  await Promise.all(notify.map(async container => await container.kill({ signal: 'HUP' })))
+}
+
+async function generateAllFiles () {
+  console.log('Getting list of containers')
+  const containers = await docker.container.list()
+  await Promise.all(containers.map(async container => {
+    if ('proxy.hosts' in container.data.Labels) {
+      console.log(`Found container for proxying ${container.data.Names[0]}`)
+      await generateHostFile(container)
+    }
+  }))
+}
+
+async function generateHostFile (container) {
+  // console.log(container.data.NetworkSettings.Networks)
+  const serverNamesArray = container.data.Labels['proxy.hosts'].split(',')
+  const serverNames = serverNamesArray.join('  ')
+  const containerPort = Number(container.data.Labels['proxy.port']) || 80
+  // External
+  let proxyPass
+  let remoteIp
+
+  const isHostNetworking = 'host' in container.data.NetworkSettings.Networks
+
+  if (config.destination) {
+    // External
+    const ports = container.data.Ports.filter(port => port.PrivatePort === containerPort)
+    // If there's a private port that matches our container port, then we grab the public port
+    // Else, we use the given container port as it's probably in host networking mode
+    const hostPort = ports.length > 0 ? ports[0].PublicPort : containerPort
+
+    proxyPass = `${config.destination}:${hostPort}`
+    remoteIp = config.destination
+  } else if (isHostNetworking) {
+    // Local host networking
+    // Do some hackery to get the host IP
+    const hostIp = execSync("route | awk '/^default/ { print $2 }'").toString().trim()
+    proxyPass = `http://${hostIp}:${containerPort}`
+  } else {
+    // Local and in same network
+    proxyPass = `${container.data.Names[0].split('/')[1]}:${containerPort}`
+  }
+  const templateFile = 'proxy.template' in container.data.Labels ? container.data.Labels['proxy.template'] : config.template
+  const template = (await fs.readFile(templateFile)).toString()
+  const vhost = template.interpolate({
+    server_names: serverNames,
+    proxy_pass: proxyPass,
+    is_public: 'proxy.isPublic' in container.data.Labels ? 'allow 0.0.0.0/0;' : '',
+    single_server_name: serverNamesArray[0],
+    remote_ip: remoteIp
+  })
+  const vhostFile = `${path.join(config.conf_dir, serverNamesArray[0])}.${config.suffix}.conf`
+  console.log(`Writing new vhost file -> ${vhostFile} for ${serverNamesArray[0]} @ ${proxyPass} (Public: ${'proxy.isPublic' in container.data.Labels})`)
+  await fs.writeFile(vhostFile, vhost)
+}
+
+// eslint-disable-next-line no-extend-native
 String.prototype.interpolate = function (params) {
-    const names = Object.keys(params);
-    const vals = Object.values(params);
-    return new Function(...names, `return \`${this}\`;`)(...vals);
+  const names = Object.keys(params)
+  const vals = Object.values(params)
+  // eslint-disable-next-line no-new-func
+  return new Function(...names, `return \`${this}\`;`)(...vals)
 }
 
 start()
